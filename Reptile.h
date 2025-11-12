c5c2cbcbb9fd8624e95729548443e0af4030f205
Baraa Tantawy <baraaibrahim171@gmail.com>
2025-11-12 15:07:36 +0200

    Define Reptile class inheriting from Animal

--- Reptile.h
+++ Reptile.h
@@ -5,7 +5,17 @@
 
 //define reptile class here
 
+class Reptile:public Animal {
+private:
+    bool isVenomous;
+    public:
+    Reptile();
+    Reptile(bool isVenomous , string name , int age , bool isHungry);
+    ~Reptile();
+    void setVenomous(bool isVenomous);
+    bool getVenomous();
 
+};
 
 
 
