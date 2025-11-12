3a8f51bf5db1a73ceb5db513f0786a1f71786214
Baraa Tantawy <baraaibrahim171@gmail.com>
2025-11-12 15:01:20 +0200

    Update Enclosure.h

--- Enclosure.h
+++ Enclosure.h
@@ -6,7 +6,24 @@
 //define enclosure class here 
 
 
-
+class Enclosure {
+private:
+    Animal* a;
+    int capacity;
+    int currentCount;
+    public:
+    Enclosure();
+    Enclosure(Animal* a, int capacity , int currentCount);
+    ~Enclosure();
+    void setAnimal(Animal* a);
+    void setCapacity(int capacity);
+    void setCurrentCount(int currentCount);
+    Animal* getAnimal();
+    int getCapacity();
+    int getCurrentCount();
+    void addAnimal(Animal* a);
+    void displayAnimals();
+};
 
 
 
