c716ce3e8abc3b9bfd57ed77099d95d824f07168
Baraa Tantawy <baraaibrahim171@gmail.com>
2025-11-12 15:08:02 +0200

    Define Visitor class with member functions

--- Visitor.h
+++ Visitor.h
@@ -5,7 +5,21 @@
 using namespace std;
 
 //define visitor class here
+class Visitor {
+private:
+    string visitorName;
+    int ticketsBought;
+    public:
+    Visitor();
+    Visitor(string visitorName, int ticketsBought);
+   ~Visitor();
+    void setVisitorName (string visitorName);
+    void setTicketsBought(int ticketsBought);
+    string getVisitorName();
+    int getTicketsBought();
+    void displayInfo();
 
+};
 
 
 
