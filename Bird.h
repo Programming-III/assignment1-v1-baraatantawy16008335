c1c5aabcad5b627967da897a54959d066e1df30b
Baraa Tantawy <baraaibrahim171@gmail.com>
2025-11-12 15:00:55 +0200

    Update Bird.h

--- Bird.h
+++ Bird.h
@@ -5,7 +5,16 @@
 
 //define class bird here 
 
-
+class Bird : public Animal {
+private:
+    float wingSpan;
+    public:
+    Bird();
+    Bird(float wingSpan , string name, int age , bool isHungry);
+    ~Bird();
+    void setWingSpan(float wingSpan);
+    float getWingSpan();
+};
 
 
 
